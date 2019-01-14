c1435e04f9a3d47cccdc5a8b642bd44ad9cfb819
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-14 13:37:08 -0800

    Move canvas pieces to own component

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Piece from '../Piece/Piece.js';
 
 class Canvas extends Component {
   render() {
     var bodyBackgroundColor = this.props.style['body-background-color'];
     return (
       <div className="block" style={{"backgroundColor": bodyBackgroundColor}}>
-        <div>MAIN TITLE</div>
-        <div>SOME CONTENT</div>
+        <Piece/>
+        <Piece/>
       </div>
     );
   }
