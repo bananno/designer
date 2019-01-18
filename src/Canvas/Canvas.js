216863e1da606a769cf1a610b1e5088e0ee064bd
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-18 13:13:05 -0800

    Refactor

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import Piece from './Piece.js';
 
 class Canvas extends Component {
+  mapPieces = (thisPiece, i) => {
+    return (
+      <Piece data={thisPiece} key={i} editPieceText={this.props.editPieceText}
+        state={this.props.state}
+        setState={this.props.setState}
+        showTools={this.props.showTools}
+        reorderPieceItems={this.props.reorderPieceItems}
+      />
+    );
+  }
+
   render() {
     var bodyBackgroundColor = this.props.style['body-background-color'];
     return (
       <div id="canvas" style={{"backgroundColor": bodyBackgroundColor}}>
-        {this.props.pieces.map((thisPiece, i) => {
-          return <Piece data={thisPiece} key={i} editPieceText={this.props.editPieceText}
-            state={this.props.state}
-            setState={this.props.setState}
-            showTools={this.props.showTools}
-            reorderPieceItems={this.props.reorderPieceItems}/>
-        })}
+        {this.props.pieces.map(this.mapPieces)}
       </div>
     );
   }
