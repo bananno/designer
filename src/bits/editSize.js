3f1511e44ba0290a67472a379d73b45fafb354ab
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-19 14:07:23 -0800

    Add save and reset functionality

--- src/bits/editSize.js
+++ src/bits/editSize.js
@@ -17,18 +17,22 @@ const editSize = (props) => {
     props.changePiece('widthInput', newValue);
   }
 
+  const save = () => {
+    props.changePiece('height', props.piece.heightInput);
+    props.changePiece('width', props.piece.widthInput);
+  }
+
+  const reset = () => {
+    props.changePiece('heightInput', props.piece.height);
+    props.changePiece('widthInput', props.piece.width);
+  }
+
   return (
     <div>
-      height:
-      <input className="edit-size"
-        value={props.piece.heightInput}
-        onChange={editHeight}/>
-      width:
-      <input className="edit-size"
-        value={props.piece.widthInput}
-        onChange={editWidth}/>
-      <button>save</button>
-      <button>cancel</button>
+      height: <input className="edit-size" value={props.piece.heightInput} onChange={editHeight}/>
+      width: <input className="edit-size" value={props.piece.widthInput} onChange={editWidth}/>
+      <button onClick={save}>save</button>
+      <button onClick={reset}>cancel</button>
     </div>
   );
 };
