3ce8b3a3e761a887b257f35c769be6d62820485a
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-01 13:02:30 -0800

    Add style to content boxes

--- src/pieces/content.js
+++ src/pieces/content.js
@@ -4,9 +4,15 @@ const content = (props) => {
   let className = props.classNames;
   let pieceStyle = {};
 
+  pieceStyle.width = '33%';
+
   return (
     <div className={className.join(' ')} style={pieceStyle}>
-      CONTENT HERE
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus hendrerit id tortor
+        eget feugiat. Fusce at urna tincidunt, gravida tellus eget, egestas nulla. Praesent
+        laoreet augue purus, nec ornare libero finibus a. Vivamus eu dapibus sem, at porta magna.
+      </p>
       {props.deleteButton}
       {props.editButtonStart}
       <div className="piece-tools">
