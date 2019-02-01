c82071ae57cce7285a9be1bd382ca5569802a3de
Anna Peterson <peterson.anna.c@gmail.com>
2019-02-01 12:34:30 -0800

    Begin adding options for editing canvas sections

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
 class Section extends Component {
   state = {
+    showEditTools: false
+  };
+
+  showEditTools = () => {
+    this.setState({
+      showEditTools: true
+    });
+  };
+
+  hideEditTools = () => {
+    this.setState({
+      showEditTools: false
+    });
   };
 
   render() {
+    let className = 'canvas-section editing-' + this.state.showEditTools;
+
     return (
-      <div className="canvas-section">
+      <div className={className}>
+        <div>
+          <button onClick={this.showEditTools} className="edit-start-button">edit section</button>
+        </div>
+        <div className="edit-box">
+          <button onClick={this.hideEditTools} className="done-button">done</button>
+        </div>
         {this.props.children}
       </div>
     );
