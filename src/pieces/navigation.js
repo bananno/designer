a9b379b63cd0ed0ca8b9cda0361fbba5938d8125
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-31 16:24:47 -0800

    Add edit options for navigation

--- src/pieces/navigation.js
+++ src/pieces/navigation.js
@@ -34,6 +34,33 @@ const navigation = (props) => {
 
   let count = -1;
 
+  const togglePieceTools = () => {
+    let newState = props.state;
+
+    newState.pieces = newState.pieces.map(thisPiece => {
+      if (thisPiece === props.piece) {
+        thisPiece.showTools = !thisPiece.showTools;
+      }
+      return thisPiece;
+    });
+
+    props.setState(newState);
+  };
+
+  const getPieceTools = () => {
+    if (!props.piece.showTools) {
+      return (
+        <button onClick={togglePieceTools} className="edit-piece">edit navigation</button>
+      );
+    }
+
+    return (
+      <div className="piece-tools">
+        <button onClick={togglePieceTools}>done</button>
+      </div>
+    );
+  };
+
   return (
     <div className="piece navigation">
       <ul>
@@ -47,10 +74,8 @@ const navigation = (props) => {
           }
           return (<li key={i} draggable="true" onDragStart={getDragStart(count)} className="item">{text}</li>);
         })}
-        <li>
-          <button className="edit-piece">edit navigation</button>
-        </li>
       </ul>
+      {getPieceTools()}
       {props.children}
     </div>
   );
